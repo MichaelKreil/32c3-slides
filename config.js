3913a523d942a49bb2011f11d2ce3da5b79c28f2
Michael Kreil <github@michael-kreil.de>
2015-12-27 20:42:07 +0100

    move folders

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-	statusFile: path.resolve(__dirname, 'videos.json'),
-	videoFolder: path.resolve(__dirname, 'videos'),
-	stripFolder: path.resolve(__dirname, 'strips')
+	statusFile:  path.resolve(__dirname, 'data/videos.json'),
+	videoFolder: path.resolve(__dirname, 'data/videos'),
+	stripFolder: path.resolve(__dirname, 'data/strips')
 }
